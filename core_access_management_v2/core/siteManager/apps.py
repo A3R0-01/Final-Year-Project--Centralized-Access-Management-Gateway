f20b493c736a864cc29fb82d7e4306af82d3d6e0
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-01-24 16:59:36 +0200

    SiteManager: models Update

--- core_access_management_v2/core/siteManager/apps.py
+++ core_access_management_v2/core/siteManager/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class SitemanagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'siteManager'
+    name = 'core.siteManager'
+    label = 'siteManager'
--- core_access_management_v2/core/siteManager/models.py
+++ core_access_management_v2/core/siteManager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from core.abstract.models import AbstractManager, AbstractModel
+from rest_framework.exceptions import PermissionDenied
 
 # Create your models here.
+class SiteManagerManager(AbstractManager):
+
+    def create(self, **kwargs):
+        if self.all().count() > 0:
+            raise PermissionDenied('There Should Only Be One Site Manager.')
+        return super().create(**kwargs)
+    pass
+
+class SiteManager(AbstractModel, AbstractBaseUser):
+    ManagerUserName = models.CharField(unique=True, max_length=100)
+    Citizen = models.ForeignKey(to='citizen.Citizen', on_delete=models.PROTECT)
+    FirstEmail = models.EmailField(unique=True)
+    SecondEmail = models.EmailField(unique=True)
+
+    objects : SiteManagerManager = SiteManagerManager()
+
+    def __str__(self):
+        return f'Site Manager Account: {self.ManagerUserName}, {self.FirstEmail}, {self.SecondEmail}'
+
