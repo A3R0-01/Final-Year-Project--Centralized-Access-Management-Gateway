95d1e83dd5c13bd1df315ee5534cb8b3f91c39ba
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-01-27 11:51:39 +0200

    Routers: SiteManager and SiteManager Auth

--- core_access_management_v2/core/routers.py
+++ core_access_management_v2/core/routers.py
@@ -1,14 +1,19 @@
 from rest_framework import routers
 from core.citizen.viewsets import CitizenViewSet
-from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
+from core.siteManager.viewsets import SiteManagerModelViewSet
+from core.auth.viewsets import RegisterViewSet, LoginCitizenViewSet, RefreshViewSet, LoginSiteManagerViewSet
 
 
 router  = routers.SimpleRouter()
 
 router.register(r'citizen', CitizenViewSet, basename='citizen')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
-router.register(r'auth/login', LoginViewSet, basename='auth-login')
-router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+router.register(r'auth/login', LoginCitizenViewSet, basename='auth-login-citizen')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh-citizen')
+
+# SiteManager
+router.register(r'admin/siteManager', SiteManagerModelViewSet, basename='siteManager')
+router.register(r'admin/login', LoginSiteManagerViewSet, basename='auth-login-sitemanager')
 
 urlPatterns = [
     *router.urls,
