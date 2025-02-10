43bac6baf0e200ce375876014b1dd74af1029a9a
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-02-10 22:24:33 +0200

    Rumentary Server

---
+++ central-gateway/RudiServer copy/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	CreateSecondClient()
+}
+
+func CreateClient() {
+	http.HandleFunc("/service", handler)
+	http.HandleFunc("/service/file", handler2("131. Anger x And x Light.mkv"))
+	log.Fatal(http.ListenAndServe(":8002", nil))
+}
+func CreateSecondClient() {
+	http.HandleFunc("/stuff", handler3)
+	http.HandleFunc("/stuff/file", handler2("131. Anger x And x Light.mkv"))
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+func handler3(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index copy.html")
+}
+func handler2(filename string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filename)
+	}
+}
