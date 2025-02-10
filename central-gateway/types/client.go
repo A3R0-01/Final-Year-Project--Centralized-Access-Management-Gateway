d880f8ed8e9c72fc081b633023981ae85c5ec586
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-02-10 22:25:34 +0200

    Types

---
+++ central-gateway/types/client.go
@@ -0,0 +1,24 @@
+package types
+
+import (
+	"net/http"
+	"net/url"
+)
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Endpoint struct {
+	ServiceName     string
+	MachineName     string
+	URL             *url.URL
+	RestrictMethods bool
+	RestrictPath    bool
+	FixedPath       string
+	Methods         []string
+}
+
+type AssociationClient struct {
+	Endpoints []Endpoint `json:"endpoints"`
+	Service   string     `json:"company"`
+	Type      string     `json:"http"`
+}
---
+++ central-gateway/types/users.go
@@ -0,0 +1,16 @@
+package types
+
+type User struct {
+}
+
+type Administrator struct {
+}
+
+type Grantee struct {
+}
+
+type SiteManager struct {
+}
+
+type UserLogin struct {
+}
