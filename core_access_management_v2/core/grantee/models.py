98302d4b3ba74d9fd6f55641bb5449e4b1386762
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-02-11 12:01:44 +0200

    Grantee: association field

--- core_access_management_v2/core/grantee/models.py
+++ core_access_management_v2/core/grantee/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from core.abstract.models import AbstractModel, AbstractManager
 
 class GranteeManager(AbstractManager):
+
+    def create(self, **kwargs):
+        if hasattr(kwargs['Administrator'], 'department'):
+            if kwargs["Association"].Department == kwargs['Administrator'].department:
+                return super().create(**kwargs)
     pass
 
 # Create your models here.
@@ -10,6 +15,7 @@ class Grantee(AbstractBaseUser, AbstractModel):
     GranteeUserName = models.CharField(max_length=50, unique=True)
     Citizen = models.OneToOneField(to='citizen.Citizen', on_delete=models.PROTECT)
     Administrator = models.ForeignKey(to='administrator.Administrator', on_delete=models.PROTECT)
+    Association = models.ForeignKey(to='association.Association', on_delete=models.DO_NOTHING)
     FirstEmail = models.EmailField(unique=True)
     SecondEmail = models.EmailField(unique=True, null=True)
 
