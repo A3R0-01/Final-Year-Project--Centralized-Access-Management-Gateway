91196e44a8e60d4acd828fc2a36d36ad80997da3
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-02-17 11:15:02 +0200

    Staff ViewSets

--- core_access_management_v2/core/request/viewsets.py
+++ core_access_management_v2/core/request/viewsets.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
-from core.abstract.viewset import AbstractModelViewSet
-from .serializers import CitizenRequestSerializer
+from rest_framework.exceptions import MethodNotAllowed
+from core.abstract.viewset import AbstractModelViewSet, AbstractGranteeModelViewSet, AbstractAdministratorModelViewSet, AbstractSiteManagerModelViewSet
+from core.department.models import Department
+from core.association.models import Association
+from .serializers import CitizenRequestSerializer, GranteeRequestSerializer, AdministratorRequestSerializer, SiteManagerRequestSerializer
 # Create your views here.
 
 class CitizenRequestViewSet(AbstractModelViewSet):
     serializer_class = CitizenRequestSerializer
-    http_method_names = ('get', 'post', 'patch', 'delete')
+    http_method_names = ('get', 'post', 'patch')
+
+    def get_queryset(self):
+        return self.serializer_class.Meta.model.objects.filter(Citizen=self.request.user)
+
+class GranteeRequestViewSet(AbstractGranteeModelViewSet):
+    serializer_class = GranteeRequestSerializer
+    http_method_names = ('get')
+
+    def get_queryset(self):
+        if hasattr(self.request.user, 'grantee'):
+            return self.serializer_class.Meta.model.objects.filter(Association=self.request.user.grantee.Association)
+        else:
+            raise MethodNotAllowed()
+
+class AdministratorRequestViewSet(AbstractAdministratorModelViewSet):
+    serializer_class = AdministratorRequestSerializer
+    http_method_names = ('get')
+
+    def get_queryset(self):
+        if hasattr(self.request.user, 'administrator'):
+            department = Department.objects.get(Administrator=self.request.user.administrator)
+            associations = Association.objects.filter(Department=department)
+            return self.serializer_class.Meta.model.objects.filter(Service__Association__in=associations)
+        raise MethodNotAllowed()
+
+class SiteManagerRequestViewSet(AbstractAdministratorModelViewSet):
+    serializer_class = SiteManagerRequestSerializer
+    http_method_names = ('get')
+
