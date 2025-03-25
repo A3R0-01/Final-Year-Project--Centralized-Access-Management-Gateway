0465e7b3170b02d07bb7a55bf2a860dbf78efb15
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-03-25 16:33:09 +0200

    Updates: Gateway

--- central-gateway/kitGateway/main.go
+++ central-gateway/kitGateway/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"net"
+	"net/http"
 	"os"
 
+	"github.com/A3R0-01/Final-Year-Project--Centralized-Access-Management-Gateway/central-gateway/kitGateway/service"
+	"github.com/A3R0-01/Final-Year-Project--Centralized-Access-Management-Gateway/central-gateway/kitGateway/serviceEndpoint"
+	"github.com/A3R0-01/Final-Year-Project--Centralized-Access-Management-Gateway/central-gateway/kitGateway/serviceTransport"
+	"github.com/A3R0-01/Final-Year-Project--Centralized-Access-Management-Gateway/central-gateway/kitGateway/system"
 	"github.com/go-kit/kit/metrics"
 	"github.com/go-kit/kit/metrics/prometheus"
 	"github.com/go-kit/log"
@@ -11,7 +16,7 @@ import (
 )
 
 func main() {
-	httpAddres := "8020"
+	httpAddr := "8020"
 	var logger log.Logger
 	{
 		logger = log.NewLogfmtLogger(os.Stderr)
@@ -29,7 +34,20 @@ func main() {
 	}
 
 	var (
-		httpListener, err = net.Listen("tcp", httpAddres)
+		httpListener, err = net.Listen("tcp", httpAddr)
+		server            = system.NewServer()
+		basicServices     = service.New(logger, server)
+		endPoints         = serviceEndpoint.New(basicServices, logger, duration)
+		httpHandler       = serviceTransport.NewHTTPHandler(endPoints, server, logger)
 	)
 
+	if err != nil {
+		logger.Log("transport", "HTTP", "during", "Listen", "err", err)
+		os.Exit(1)
+	}
+	logger.Log("transport", "HTTP", "addr", httpAddr)
+	err = http.Serve(httpListener, httpHandler)
+	if err != nil {
+		panic(err)
+	}
 }
