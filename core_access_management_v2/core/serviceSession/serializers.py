8276babe46d1c7fdf330ddde9bad46a65051ff6f
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2025-05-08 16:37:07 +0200

    ServiceSession: Viewsets and Serializers

--- core_access_management_v2/core/serviceSession/serializers.py
+++ core_access_management_v2/core/serviceSession/serializers.py
@@ -2,17 +2,51 @@ from rest_framework import serializers
 from core.abstract.serializers import AbstractModelSerializer
 from core.citizen.models import Citizen as CitizenModel
 from core.publicService.models import PublicService
+from core.citizen.serializers import ServiceSessionCitizenSerializer
+from core.publicService.serializers import ServiceSessionPublicServiceSerializer
 from .models import ServiceSession
 
-class ManagerServiceSessionSerializer(AbstractModelSerializer):
+class GranteeServiceSessionSerializer(AbstractModelSerializer):
+
     Citizen = serializers.SlugRelatedField(queryset=CitizenModel.objects.all(), slug_field="PublicId")
     Service = serializers.SlugRelatedField(queryset=PublicService.objects.all(), slug_field="PublicId")
+    Expired = serializers.SerializerMethodField()
+
+    def get_Expired(self, serviceSession : ServiceSession) -> bool:
+        return serviceSession.expired
+
+    def to_representation(self, instance) -> dict:
+        data = super().to_representation(instance)
+        data['Citizen'] = ServiceSessionCitizenSerializer(instance.Citizen).data
+        data['Service'] = ServiceSessionPublicServiceSerializer(instance.Service).data
+        return data
+    class Meta:
+        model : ServiceSession = ServiceSession
+        fields : list[str] = [
+            'id', 'Citizen', 'Service', 'EnforceExpiry', 'Expired', 'Created', 'Updated'
+        ]
+        read_only_fields : list[str] = [
+            'id', 'Citizen', 'Service', 'EnforceExpiry', 'Expired', 'Created', 'Updated'
+        ]
+
+class AdministratorServiceSessionSerializer(GranteeServiceSessionSerializer):
 
     class Meta:
         model : ServiceSession = ServiceSession
         fields : list[str] = [
-            'id', 'Citizen', 'Service', 'Created', 'Updated'
+            'id', 'Citizen', 'Service', 'EnforceExpiry', 'Expired', 'Created', 'Updated'
         ]
         read_only_fields : list[str] = [
-            'id', 'Created'
+            'id', 'Citizen', 'Service', 'EnforceExpiry', 'Expired', 'Created', 'Updated'
+        ]
+
+class SiteManagerServiceSessionSerializer(GranteeServiceSessionSerializer):
+
+    class Meta:
+        model : ServiceSession = ServiceSession
+        fields : list[str] = [
+            'id', 'Citizen', 'Service', 'EnforceExpiry', 'Expired', 'Created', 'Updated'
+        ]
+        read_only_fields : list[str] = [
+            'id', 'Created', 'Expired', 'Updated'
         ]
--- core_access_management_v2/core/serviceSession/viewset.py
+++ core_access_management_v2/core/serviceSession/viewset.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from core.abstract.viewset import AbstractAdministratorModelViewSet, AbstractGranteeModelViewSet, AbstractSiteManagerModelViewSet
+from .serializers import SiteManagerServiceSessionSerializer, AdministratorServiceSessionSerializer, GranteeServiceSessionSerializer
 # Create your views here.
+class GranteeServiceSessionViewSet(AbstractGranteeModelViewSet):
+    serializer_class = GranteeServiceSessionSerializer
+    http_method_names = ('get')
+
+
+class AdministratorServiceSessionViewSet(AbstractAdministratorModelViewSet):
+    serializer_class = AdministratorServiceSessionSerializer
+    http_method_names = ('get')
+
+class SiteManagerServiceSessionViewSet(AbstractSiteManagerModelViewSet):
+    serializer_class = SiteManagerServiceSessionSerializer
+    http_method_names = ('get', 'post', 'patch')
